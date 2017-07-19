655a342da875ec21763893705f750bba3a335175
kritpetra <krit.petra@gmail.com>
2017-07-18 23:37:54 -0700

    distinguished between === and == operators

--- R/comparison.R
+++ R/comparison.R
@@ -1,12 +1,36 @@
-`==.measr` = function(e1, e2) {
+#' Strict comparison
+#'
+#' Returns \code{TRUE} if and only if the objects' values and errors are equal.
+#'
+#' @param e1 First \code{measr} object
+#' @param e2 Second \code{measr} object
+#' @return
+#' Boolean representing if the objects represent two measurements that have the 
+#' same mean and measurement error.
+`===.measr` = function(e1, e2) {
   return(e1$val == e2$val && e1$err == e2$err)
 }
 
-`!=.measr` = function(e1, e2) {
+#' @rdname `===.measr`
+`!==.measr` = function(e1, e2) {
   return(e1$val != e2$val || e1$err != e2$err)
 }
 
-# Checks if the values are equal with `conf`% confidence, using a two-sample t-test
-`%==%.measr` = function(e1, e2, conf = 0.95) {
-  # TODO
+
+#' Measurement comparison
+#'
+#' Compares two \code{measr} objects and returns \code{TRUE} if the two have 
+#' standard uncertainty ranges that overlap.
+#' 
+#' @param e1 First \code{measr} object
+#' @param e2 Second \code{measr} object
+#' @return
+#' Whether or not the two uncertainty ranges overlap 
+`==.measr` = function(e1, e2) {
+  return(e1$val - e1$err <= e2$val + e2$err && e2$val - e2$err <= e1$val + e1$err)
+}
+
+#' @rdname `==.measr`
+`!=.measr` = function(e1, e2) {
+  !(e1 == e2)
 }
